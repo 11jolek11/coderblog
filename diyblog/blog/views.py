ec0a4d5a4c9c677d1b4d38eb6b57f6add09e4422
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-02-15 20:17:00 +0100

    Index view update Fix in href for language template engine

--- diyblog/blog/views.py
+++ diyblog/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from blog.models import Blog, BlogAuthor
 
 # Create your views here.
@@ -7,4 +8,14 @@ def index(request):
     # TODO: fill context
     count_blog = Blog.objects.count()
     count_authors = BlogAuthor.objects.count()
-    return render(request, 'index.html')
+
+    context = {
+        'count_blog': count_blog,
+        'count_authors': count_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+
+class BlogListView(generic.ListView):
+    # model = Book
+    pass
