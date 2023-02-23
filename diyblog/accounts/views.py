d9f3b04184786655bb3a0296bf67004af33068ae
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-02-23 19:44:47 +0100

    Custom SignUp form Custom form for signup

--- diyblog/accounts/views.py
+++ diyblog/accounts/views.py
@@ -1,12 +1,19 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from accounts.forms import SignUpForm
+from typing import Any
+from django import http
 
 
 # Create your views here.
-class SignUpCreateView(generic.CreateView):
-    # TODO: Create your own form for User registration
-    form_class = UserCreationForm
+class SignUpCreateView(generic.CreateView, LoginRequiredMixin):
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+    # Prevent logged user to register
+    def dispatch(self, request: http.HttpRequest, *args: Any, **kwargs: Any) -> http.HttpResponse:
+        if request.user.is_authenticated:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
